936493963327aba02459e146c0191b4e294d13f6
Scott Kirschner <scott.kirschner@perficient.com>
2020-12-08 09:21:23 -0500

    fixing s3 issues

--- deploy/s3.tf
+++ deploy/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "app_public_files" {
-  bucket        = "${local.prefix}-files-thiswilldefinitelybeunique"
+  bucket        = "${local.prefix}-files"
   acl           = "public-read"
   region        = "ap-northeast-1"
   force_destroy = true
