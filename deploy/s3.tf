fc1aea727e8495042e6a114ae2624338799417b7
Scott Kirschner <scott.kirschner@perficient.com>
2020-12-08 09:38:35 -0500

    got rid of s3 location

--- deploy/s3.tf
+++ deploy/s3.tf
@@ -1,6 +1,5 @@
 resource "aws_s3_bucket" "app_public_files" {
   bucket        = "${local.prefix}-files"
   acl           = "public-read"
-  region        = "ap-northeast-1"
   force_destroy = true
 }
